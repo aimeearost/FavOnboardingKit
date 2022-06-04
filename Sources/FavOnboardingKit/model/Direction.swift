9b943112af5aa0caee64a0fe5e405501cfb8d655
Aimee Arost <aimee@aimeearost.com>
2022-06-03 17:11:38 -0700

    complete project and add README.md file

---
+++ Sources/FavOnboardingKit/model/Direction.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aimee Arost on 6/2/22.
+//
+
+import Foundation
+
+enum Direction {
+    case left
+    case right
+}
---
+++ Sources/FavOnboardingKit/model/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aimee Arost on 5/31/22.
+//
+
+import Foundation
+import UIKit
+
+
+public struct Slide {
+    public let image: UIImage
+    public let title: String
+
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+
+}
+
